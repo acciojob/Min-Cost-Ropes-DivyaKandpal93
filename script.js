cfb71873100a69c60ad345f9ed7205a9b3266d50
Divya kandpal <80275853+DivyaKandpal93@users.noreply.github.com>
2024-02-18 15:52:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	arr.sort((a,b) => a-b);
+	let totalCost =0;
+	while (arr.length>1) {
+		const rope1 = arr.shift();
+	const rope2 = arr.shift();
+	mergeCost = rope1 + rope2;
+		totalCost += mergeCost;
+	arr.push(mergeCost);
+		arr.sort((a,b) => a-b);
+	}
+  return totalCost;
 }
 
 module.exports=mincost;
